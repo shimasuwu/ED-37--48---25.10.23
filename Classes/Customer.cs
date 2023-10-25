b1397e86d5ecefa4ae1940e5ee457eb03b036f1a
Tugberk Gungor <tgbrkgng@hotmail.com>
2023-10-25 23:12:57 +0300

    Add project files.

---
+++ Classes/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Customer
+    {
+
+        ////field
+        //public string FirstName;
+
+        //prop
+        public int ID { get; set; }
+        public string city { get; set; }
+        public string fn { get; set; }
+        public string ln { get; set; }
+
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+            
+            Console.WriteLine();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer cstm = new Customer();
+            cstm.ID = 1;
+            cstm.city = "istanbul";
+            cstm.fn = "tugberk";
+            cstm.ln = "gungor";
+
+
+            Console.Read();
+        }
+
+        class CustomerManager 
+        {
+            public void Add()
+            {
+                Console.WriteLine("Customer Added.");
+            }
+            public void Update()
+            {
+                Console.WriteLine("Customer Updated.");
+            }
+        }
+ 
+
+    }
+}
